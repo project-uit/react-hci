a03823ae5aa9a91b5304ea9df07d87ef57def840
Iga Seha <quochunghero@gmail.com>
2018-12-30 15:00:22 +0700

    add icon, navigation

--- mobile-app/NativeApp/App.js
+++ mobile-app/NativeApp/App.js
@@ -7,23 +7,49 @@
  */
 
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View, DrawerLayoutAndroid, ToolbarAndroid } from 'react-native';
-import DrawerComponent from './Component/Drawer/drawer';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import { Platform, StyleSheet } from 'react-native';
+import { Container, Header, Left, Body, Right, Button, Icon, Title, Drawer } from 'native-base';
+import HeaderComponent from './Header/header';
+import SideBar from './Component/SideBar/sidebar';
+// const instructions = Platform.select({
+//   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+//   android:
+//     'Double tap R on your keyboard to reload,\n' +
+//     'Shake or press menu button for dev menu',
+// });
 
 type Props = {};
 export default class App extends Component<Props> {
+  closeDrawer() {
+    this.drawer._root.close()
+  };
+  openDrawer() {
+    this.drawer._root.open()
+  };
   render() {
 
     return (
-        <DrawerComponent />
 
+      <Drawer
+        ref={(ref) => { this.drawer = ref; }}
+        content={<SideBar navigator={this.navigator} />}
+        onClose={() => this.closeDrawer()} >
+        <Header style={{ backgroundColor: '#0F0F0F' }}>
+          <Left>
+            <Button transparent onPress={() => this.openDrawer()} style={{}}>
+              <Icon name='ios-menu' size={30} />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Shoe style</Title>
+          </Body>
+          <Right>
+            {/* <Button transparent onPress={()=> this.openDrawer()}>
+                <Icon name='ios-menu' size={30} />
+              </Button> */}
+          </Right>
+        </Header>
+      </Drawer>
     );
   }
 }
