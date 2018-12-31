b64b82a45eeacc3ab2080281b7519d3b28f22c15
Iga Seha <quochunghero@gmail.com>
2018-12-31 21:21:41 +0700

    update responsive litsshoe

--- src/List/DataShoe.js
+++ src/List/DataShoe.js
@@ -8,13 +8,14 @@ import { Affix, Button, Tooltip } from 'antd';
 import { Breadcrumb } from 'antd';
 import { Link } from 'react-router-dom';
 import { Input } from 'antd';
-
+import Scroll from 'react-scroll';
 
 const {
     Sider,
 } = Layout;
 const Option = Select.Option;
 const Panel = Collapse.Panel;
+var Element = Scroll.Element;
 export const ListShoeM = (
 
     <Row>
@@ -27,85 +28,92 @@ export const ListShoeM = (
                     </Tooltip>
                     <Sider
                         theme="light"
+                        className="rounded-lg shadow"
                         style={{ width: '100%' }}
                         breakpoint="lg"
                         collapsedWidth="0"
                         onBreakpoint={(broken) => { console.log(broken); }}
                         onCollapse={(collapsed, type) => { console.log(collapsed, type); }}
                     >
-                        <Collapse defaultActiveKey={['4', '1']}>
-                            <Panel header="Sắp xếp theo" key="4">
-                                <Row>
-                                    <Col span={24}><Checkbox value="1">Bán chạy</Checkbox></Col>
-                                    <Col span={24}><Checkbox value="2">Phổ biến</Checkbox></Col>
-                                    <Col span={24}><Checkbox value="3">Hot trending</Checkbox></Col>
-                                </Row>
-                            </Panel>
-                            <Panel header="Giới tính" key="1">
-                                <Checkbox.Group style={{ width: '100%' }}>
+                        <Element id="containerElement" style={{
+                           
+                            height: 400,
+                            overflow: 'scroll',
+                        }}>
+                            <Collapse defaultActiveKey={['4', '1']}>
+                                <Panel header="Sắp xếp theo" key="4">
                                     <Row>
-                                        <Col span={24}><Checkbox value="A">Nam</Checkbox></Col>
-                                        <Col span={24}><Checkbox value="B">Nữ</Checkbox></Col>
-                                        <Col span={24}><Checkbox value="C">Unisex</Checkbox></Col>
+                                        <Col span={24}><Checkbox value="1">Bán chạy</Checkbox></Col>
+                                        <Col span={24}><Checkbox value="2">Phổ biến</Checkbox></Col>
+                                        <Col span={24}><Checkbox value="3">Hot trending</Checkbox></Col>
                                     </Row>
-                                </Checkbox.Group>
-                            </Panel>
-                            <Panel header="Loại giày" key="3">
-                                <Row>
-                                    <Col span={24}><Checkbox value="1A">Running</Checkbox></Col>
-                                    <Col span={24}><Checkbox value="B2">Traning</Checkbox></Col>
-                                    <Col span={24}><Checkbox value="C3">Classic</Checkbox></Col>
-                                    <Col span={24}><Checkbox value="Cs">Tennis</Checkbox></Col>
-                                    <Col span={24}><Checkbox value="C123s">Basketball</Checkbox></Col>
-                                    <Col span={24}><Checkbox value="Cz">Football</Checkbox></Col>
-                                    <Col span={24}><Checkbox value="Cghj">LifeStyle</Checkbox></Col>
-                                    <Col span={24}><Checkbox value="C123">Low top</Checkbox></Col>
-                                    <Col span={24}><Checkbox value="C222">High top</Checkbox></Col>
-                                    <Col span={24}><Checkbox value="C22222">Jordan</Checkbox></Col>
-                                </Row>
-                            </Panel>
-                            <Panel header="Size" key="78">
-                                <Row>
-                                    <Col>
-                                        <div>
-                                            <label className="ant-btn ant-btn-circle">
-                                                <input type="checkbox" style={{ display: 'none' }} />
-                                            </label>
-                                            <label className="ant-btn ant-btn-circle">
-                                                <input type="checkbox" style={{ display: 'none' }} />
-                                            </label>
-                                            <label className="ant-btn ant-btn-circle">
-                                                <input type="checkbox" style={{ display: 'none' }} />
-                                            </label>
-                                            <label className="ant-btn ant-btn-circle">
-                                                <input type="checkbox" style={{ display: 'none' }} />
-                                            </label>
-                                        </div>
-                                        <div>
-                                            <label className="ant-btn ant-btn-circle">
-                                                <input type="checkbox" style={{ display: 'none' }} />
-                                            </label>
-                                            <label className="ant-btn ant-btn-circle">
-                                                <input type="checkbox" style={{ display: 'none' }} />
-                                            </label>
-                                            <label className="ant-btn ant-btn-circle">
-                                                <input type="checkbox" style={{ display: 'none' }} />
-                                            </label>
-                                            <label className="ant-btn ant-btn-circle">
-                                                <input type="checkbox" style={{ display: 'none' }} />
-                                            </label>
-                                        </div>
-                                    </Col>
+                                </Panel>
+                                <Panel header="Giới tính" key="1">
+                                    <Checkbox.Group style={{ width: '100%' }}>
+                                        <Row>
+                                            <Col span={24}><Checkbox value="A">Nam</Checkbox></Col>
+                                            <Col span={24}><Checkbox value="B">Nữ</Checkbox></Col>
+                                            <Col span={24}><Checkbox value="C">Unisex</Checkbox></Col>
+                                        </Row>
+                                    </Checkbox.Group>
+                                </Panel>
+                                <Panel header="Loại giày" key="3">
+                                    <Row>
+                                        <Col span={24}><Checkbox value="1A">Running</Checkbox></Col>
+                                        <Col span={24}><Checkbox value="B2">Traning</Checkbox></Col>
+                                        <Col span={24}><Checkbox value="C3">Classic</Checkbox></Col>
+                                        <Col span={24}><Checkbox value="Cs">Tennis</Checkbox></Col>
+                                        <Col span={24}><Checkbox value="C123s">Basketball</Checkbox></Col>
+                                        <Col span={24}><Checkbox value="Cz">Football</Checkbox></Col>
+                                        <Col span={24}><Checkbox value="Cghj">LifeStyle</Checkbox></Col>
+                                        <Col span={24}><Checkbox value="C123">Low top</Checkbox></Col>
+                                        <Col span={24}><Checkbox value="C222">High top</Checkbox></Col>
+                                        <Col span={24}><Checkbox value="C22222">Jordan</Checkbox></Col>
+                                    </Row>
+                                </Panel>
+                                <Panel header="Size" key="78">
+                                    <Row>
+                                        <Col>
+                                            <div>
+                                                <label className="ant-btn ant-btn-circle">
+                                                    <input type="checkbox" style={{ display: 'none' }} />
+                                                </label>
+                                                <label className="ant-btn ant-btn-circle">
+                                                    <input type="checkbox" style={{ display: 'none' }} />
+                                                </label>
+                                                <label className="ant-btn ant-btn-circle">
+                                                    <input type="checkbox" style={{ display: 'none' }} />
+                                                </label>
+                                                <label className="ant-btn ant-btn-circle">
+                                                    <input type="checkbox" style={{ display: 'none' }} />
+                                                </label>
+                                            </div>
+                                            <div>
+                                                <label className="ant-btn ant-btn-circle">
+                                                    <input type="checkbox" style={{ display: 'none' }} />
+                                                </label>
+                                                <label className="ant-btn ant-btn-circle">
+                                                    <input type="checkbox" style={{ display: 'none' }} />
+                                                </label>
+                                                <label className="ant-btn ant-btn-circle">
+                                                    <input type="checkbox" style={{ display: 'none' }} />
+                                                </label>
+                                                <label className="ant-btn ant-btn-circle">
+                                                    <input type="checkbox" style={{ display: 'none' }} />
+                                                </label>
+                                            </div>
+                                        </Col>
 
-                                </Row>
-                            </Panel>
-                            <Panel header="Giá bán" key="31">
-                                <Row>
-                                    <Col span={24}>Từ: <Input type="number" /></Col>
-                                    <Col span={24}>Đến: <Input type="number" /></Col>
-                                </Row>
-                            </Panel>
-                        </Collapse>
+                                    </Row>
+                                </Panel>
+                                <Panel header="Giá bán" key="31">
+                                    <Row>
+                                        <Col span={24}>Từ: <Input type="number" /></Col>
+                                        <Col span={24}>Đến: <Input type="number" /></Col>
+                                    </Row>
+                                </Panel>
+                            </Collapse>
+                        </Element>
                     </Sider>
                 </div>
             </Affix>
